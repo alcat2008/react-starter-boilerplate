ca5a550fe63a97d5d6780a32466c566ff964dc24
alcat2008 <ge_yangyang@126.com>
2016-09-10 22:47:43 +0800

    use chrome redux DevTools, remove remote-redux-devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,26 +2,37 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise';
-import devTools from 'remote-redux-devtools';
-import createLogger from 'redux-logger';
 
 import rootReducer from '../reducers';
 const middleware = [thunkMiddleware, promiseMiddleware];
 
 export default function configureStore(initialState, onComplete: ?() => void) {
   let finalCreateStore;
+  let devEnhancer;
   if (process.env.NODE_ENV === 'production') {
     finalCreateStore = applyMiddleware(...middleware)(createStore);
   } else {
+    /* eslint-disable global-require */
+    // const devTools = require('remote-redux-devtools');
+    const createLogger = require('redux-logger');
     const loggerMiddleware = createLogger({ duration: true });
     finalCreateStore = compose(
       applyMiddleware(...middleware, loggerMiddleware),
-      devTools({ realtime: true })
+      // devTools({ realtime: true })
     )(createStore);
+
+    devEnhancer = window.devToolsExtension && window.devToolsExtension();
   }
 
-  const store = finalCreateStore(rootReducer, initialState);
+  const store = finalCreateStore(rootReducer, initialState, devEnhancer);
   onComplete && onComplete();
+
+  // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
+  // if (module.hot) {
+  //   module.hot.accept('../reducers', () =>
+  //     store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
+  //   );
+  // }
 
   return store;
 }
